58b60fb9e3b38d80a68a3a0e13264a0adaecf07f
Alexander Muth <almu.hp@arcor.de>
2020-01-26 13:23:32 +0100

    Added some basic functions

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,31 @@
 #include <jni.h>
+
 #include <string>
+
+std::string jstring2string(JNIEnv *env, jstring jStr) {
+    if (!jStr)
+        return "";
+
+    const jclass stringClass = env->GetObjectClass(jStr);
+    const jmethodID getBytes = env->GetMethodID(stringClass, "getBytes", "(Ljava/lang/String;)[B");
+    const jbyteArray stringJbytes = (jbyteArray) env->CallObjectMethod(jStr, getBytes, env->NewStringUTF("UTF-8"));
+
+    size_t length = (size_t) env->GetArrayLength(stringJbytes);
+    jbyte* pBytes = env->GetByteArrayElements(stringJbytes, NULL);
+
+    std::string ret = std::string((char *)pBytes, length);
+    env->ReleaseByteArrayElements(stringJbytes, pBytes, JNI_ABORT);
+
+    env->DeleteLocalRef(stringJbytes);
+    env->DeleteLocalRef(stringClass);
+    return ret;
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_de_alex2804_libtcctest_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject /* this */,
+        jstring filesPath)
+{
+    return env->NewStringUTF(jstring2string(env, filesPath).c_str());
 }
--- app/src/main/java/de/alex2804/libtcctest/MainActivity.java
+++ app/src/main/java/de/alex2804/libtcctest/MainActivity.java
@@ -5,12 +5,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    public boolean copyAssetFolder(String srcName, String dstName) {
+        try {
+            boolean result = true;
+            String fileList[] = getAssets().list(srcName);
+            if (fileList == null)
+                return false;
+
+            if (fileList.length == 0) {
+                result = copyAssetFile(srcName, dstName);
+            } else {
+                File file = new File(dstName);
+                result = file.mkdirs();
+                for (String filename : fileList) {
+                    result = copyAssetFolder(srcName + File.separator + filename, dstName + File.separator + filename);
+                }
+            }
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean copyAssetFile(String srcName, String dstName) {
+        try {
+            InputStream in = getAssets().open(srcName);
+            File outFile = new File(dstName);
+            OutputStream out = new FileOutputStream(outFile);
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            out.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +68,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText(stringFromJNI(getFilesDir().getAbsolutePath() + "/"));
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String stringFromJNI(String filesPath);
 }
